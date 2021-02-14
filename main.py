a2abed890c6e2a2d85f5119b021fc838dc5071aa
anna-atl <kiccoro@mail.ru>
2021-02-14 10:31:21 +0100

    updates .py and created df_matches with attributes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import random
 
 def df_import():
     # import FR data (adjustments for delimeters and encoding - latin)
-    wordbook_name = "~/Dropbox/Botva/TUM/Master_Thesis/object-identification/datasets/processed_files/france_rna_processed.csv"
+    wordbook_name = "~/Dropbox/Botva/TUM/Master_Thesis/datasets/processed_files/france_rna_processed.csv"
     df = pd.read_csv(wordbook_name, encoding='latin-1', sep = ';', error_bad_lines=False)
 #    df = pd.read_csv("~/Dropbox/Botva/TUM/Master_Thesis/object-identification/datasets/raw_files/rna_waldec_20201201_dpt_01.csv", error_bad_lines=False)
 #    df = df.astype(str)
@@ -88,52 +88,113 @@ def levenstein_distance(texts_1,texts_2):
         text_1_num += 1
     return lvn_array
 
-df = df_import()
+#creating shingles_dict
+def create_shingles_dict(texts,k):
+    shingles_list = set()
 
-df_processed = df_prepare(df)
-texts = df_processed['name']
-
-n = 2
-shingles_list = set()
-
-for text in texts:
+    for text in texts:
     #    print(text)
     #    text = text.encode()
-    shingles = [text[i:i + n] for i in range(len(text) - n + 1)]
+        shingles = [text[i:i + k] for i in range(len(text) - k + 1)]
     #    print(shingles)
-    for shingle in shingles:
-        if shingle not in shingles_list:  # check, maybe if is not needed bc its a set
-            shingles_list.add(shingle)
+        for shingle in shingles:
+            if shingle not in shingles_list: #check, maybe if is not needed bc its a set
+                shingles_list.add(shingle)
 
-shingles_list = sorted(shingles_list)
-shingles_dict = dict(zip(shingles_list, range(len(shingles_list))))
+    shingles_list = sorted(shingles_list)
+    shingles_dict = dict(zip(shingles_list,range(len(shingles_list))))
+    return shingles_list, shingles_dict
 
+#converting docs to shingles
+def create_doc_shingles(texts,k):
+    docs = [[] for i in range(len(texts))]
 
+    for doc, text in zip(docs, texts):
+        shingles = [text[i:i + k] for i in range(len(text) - k + 1)]
+        for shingle in shingles:
+            doc.append(shingles_dict[shingle])
+    return docs
 
-docs = [[] for i in range(len(df_processed['name']))]
+#creating signatures array
+def create_signatures_array(docs,shingles_list,signature_size):
+    signatures = np.zeros((signature_size, len(docs))) #create an array
 
-#hashes_array = np.zeros((len(shingles_list), len(df_processed['name'])))
+    shingles_shuffled = [i for i in range(len(shingles_list))]
 
-for doc, text in zip(docs, texts):
-    shingles = [text[i:i + n] for i in range(len(text) - n + 1)]
-    for shingle in shingles:
-        doc.append(shingles_dict[shingle])
+    for signature in signatures:
+        random.shuffle(shingles_shuffled)
+        for doc_index, doc in enumerate(docs):
+            doc_a = [shingles_shuffled[i] for i in doc]
+            signature[doc_index] = min(doc_a)
+    return signatures
 
+def create_buckets(signatures,bands_number):
+    r = int(len(signatures)/bands_number) #rows per band
 
+    buckets_bands = [{} for i in range(bands_number)]
 
-signature_size = 50 #signature size
-#signatures = [[] for i in range(len(signature_size))]
+    for band, buckets in zip(range(bands_number),buckets_bands):
+        for i in range(len(signatures[0])):
+            buckets.setdefault(tuple(signatures[band*r:band*r+r,i]),[]).append(i)
+        filtered = {key:item for key, item in buckets.items() if len(item)>1}
+        buckets.clear()
+        buckets.update(filtered)
+    return buckets_bands
 
-signatures = np.zeros((signature_size, len(docs)))
+def jaccard(list1, list2):
+    intersection = len(set(list1).intersection(list2))
+    union = (len(set(list1)) + len(set(list2))) - intersection
+    return intersection/union
 
-#signatures = [[] for i in range(len(signature_size))]
+def create_matches(buckets_bands):
+    matches = {}
+    for buckets in buckets_bands:
+        for key, values_list in buckets.items():
+            for value_1 in values_list:
+                for value_2 in values_list:
+                    if value_2 > value_1 and (value_1,value_2) not in matches:
+                        matches.setdefault((value_1,value_2),[]).append(jaccard(docs[value_1], docs[value_2]))
+                        #print(value_1)
+                        #print(value_2)
+    #                    print(texts[value_1])
+    #                    print(texts[value_2])
+    #                    print(jaccard(docs[value_1], docs[value_2]))
+    return matches
 
-shingles_shuffled = [i for i in range(len(shingles_list))]
+#generate df with all potential matches
+def create_df_with_attributes(matches,texts):
+    df_matches = pd.DataFrame.from_dict(matches, orient='index')
+    df_matches['matches_tuple'] = df_matches.index
+    df_matches[['doc_1','doc_2']] = pd.DataFrame(df_matches['matches_tuple'].tolist(), index=df_matches.index)
+    df_matches = df_matches.drop(['matches_tuple'], axis=1)
+    df_matches = df_matches.reset_index(drop=True)
 
-for signature in signatures:
-    random.shuffle(shingles_shuffled)
-    for doc_index, doc in enumerate(docs):
-        doc_a = [shingles_shuffled[i] for i in doc]
-        signature[doc_index] = min(doc_a)
+    df_texts = pd.DataFrame(texts)
+    df_texts['index'] = df_texts.index
 
-print(signatures)
+    df_matches_full = pd.merge(df_matches, df_texts,  how='left', left_on=['doc_1'], right_on = ['index'])
+    df_matches_full = df_matches_full.drop(['index'], axis=1)
+    df_matches_full = pd.merge(df_matches_full, df_texts,  how='left', left_on=['doc_2'], right_on = ['index'])
+    df_matches_full = df_matches_full.drop(['index'], axis=1)
+    return df_matches_full
+
+df = df_import()
+df_processed = df_prepare(df)
+
+k = 2 #shingles size
+texts = df_processed['name']
+shingles_list, shingles_dict = create_shingles_dict(texts,k)
+
+docs = create_doc_shingles(texts,k)
+
+signature_size = 50
+signatures = create_signatures_array(docs,shingles_list,signature_size)
+
+bands_number = 5
+buckets_bands = create_buckets(signatures,bands_number)
+
+#threshold = 0.7
+matches = create_matches(buckets_bands)
+
+df_matches_full = create_df_with_attributes(matches,texts)
+print(df_matches_full)
