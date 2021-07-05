316c22b97f5b7dea7714bc13913d214ff40a0b5b
anna-atl <kiccoro@mail.ru>
2021-07-05 16:59:50 +0200

    continuing on rewriting the results comparison code

--- results_comparison.py
+++ results_comparison.py
@@ -4,22 +4,14 @@ import main_joint
 import time
 import datetime
 
-class normal_matching_params:
-    def __init__(self, matching_attribute, matching_method, attribute_threshold, attribute_weight=1):
-        self.matching_attribute = matching_attribute
-        self.matching_method = matching_method#jaccard etc?
-        self.attribute_threshold = attribute_threshold
-        self.attribute_weight = attribute_weight
-
-class matching_params:
-    def __init__(self, matching_attribute, attribute_threshold, matching_method='minhash', hash_type = 'none', hash_weight = '', attribute_weight=1, shingle_size=2, bands_number=5, signature_size=50):
+class attribute_matching_params:
+    def __init__(self, matching_attribute, matching_method, attribute_weight=1, hash_type='none', hash_weight='none', shingle_size=2, bands_number=5, signature_size=50):
         self.matching_attribute = matching_attribute
         self.matching_method = matching_method
         self.attribute_weight = attribute_weight
         self.hash_type = hash_type# token, shingle
         self.shingle_size = shingle_size# 1,2
         self.hash_weight = hash_weight# 'normal', 'frequency', 'weighted'
-        self.attribute_threshold = attribute_threshold
         self.bands_number = bands_number
         self.signature_size = signature_size
 
@@ -30,20 +22,7 @@ def import_labeled_data():
     df_labeled_data = df_labeled_data[['id_x', 'id_y', 'is_duplicate']]
     return df_labeled_data
 
-def add_results_estimation(df_all_matches, df_labeled_data):
-    labeled_positive = df_labeled_data['is_duplicate'].loc[df_labeled_data['is_duplicate'] == 2].count()
-    labeled_negative = df_labeled_data['is_duplicate'].loc[
-        (df_labeled_data['is_duplicate'] == 0) | (df_labeled_data['is_duplicate'] == 1)].count()
-    labeled_matches_count = df_labeled_data['is_duplicate'].count()
-
-    df_matches_estimation = pd.merge(df_all_matches, df_labeled_data, how='left',
-                                     left_on=['doc_1', 'doc_2'], right_on=['id_x', 'id_y'])
-    #df_matches_estimation = df_matches_estimation.drop(['id_x_x', 'id_x_y', 'id_y_x', 'id_y_y'], axis=1)
-
-    # test_mode
-    # df_matches_estimation = df_all_matches
-    # df_matches_estimation['is_duplicate'] = 0
-
+def add_results_estimation(df_matches_estimation, labeled_positive, labeled_negative, attribute_threshold):
     matches_with_labels_count = df_matches_estimation['is_duplicate'].count()
     true_positive = df_matches_estimation['is_duplicate'].loc[df_matches_estimation['is_duplicate'] == 2].count()
     false_positive = df_matches_estimation['is_duplicate'].loc[
@@ -54,12 +33,15 @@ def add_results_estimation(df_all_matches, df_labeled_data):
 
     return false_positive, false_negative, true_positive, true_negative
 
+def experiments_performance():
+    continue
+
 def finding_best_methods_for_atts():
     '''
     iterating through all matching methods for attributes for finding the best methods for each attribute
     '''
     #test_mode
-    dataset_sizes = [1000000]
+    dataset_sizes = [1000]
     #dataset_sizes = [100, 1000, 10000]
 
     #matching_attributes = ['name_clean']
@@ -81,72 +63,71 @@ def finding_best_methods_for_atts():
     #shingle_sizes = [2, 3, 4]
     shingle_sizes = [2, 3, 4]
 
+    bands_numbers = [5]
+    signature_sizes = [50]
+
     column_names = ['dataset_size', 'matching_attribute', 'attribute_weights', 'attribute_threshold', 'matching_method', 'hash_type', 'shingles_size',
                     'hash_weight', 'signature_size', 'bands_number',
                     'total_time', 'number_of_matches', 'minhash_time', 'sign_creation_time', 'false_pos_rate',
                     'false_neg_rate', 'true_pos_rate', 'true_neg_rate']
-
     df_results = pd.DataFrame(columns=column_names)
 
     df_labeled_data = import_labeled_data()
+    labeled_positive = df_labeled_data['is_duplicate'].loc[df_labeled_data['is_duplicate'] == 2].count()
+    labeled_negative = df_labeled_data['is_duplicate'].loc[(df_labeled_data['is_duplicate'] == 0) | (df_labeled_data['is_duplicate'] == 1)].count()
+    labeled_matches_count = df_labeled_data['is_duplicate'].count()
 
     for dataset_size in dataset_sizes:
         for matching_attribute in matching_attributes:
             for attribute_weight in attribute_weights:
-                for attribute_threshold in attribute_thresholds:
+#                for attribute_threshold in attribute_thresholds:
                     for matching_method in matching_methods:
-                        if matching_params[0].matching_method == 'minhash':
+                        if matching_method != 'minhash':
+                            hash_types = [0]
+                            hash_weights = [0]
+                            shingle_sizes = [0]
+                            bands_numbers = [0]
+                            signature_sizes = [0]
+                        else:
                             for hash_type in hash_types:
-                                for hash_weight in hash_weights:
-                                    if hash_type == 'shingle':
-                                        for k in shingle_sizes:
-                                            matching_params = [minhash_matching_params(matching_attribute, hash_type, hash_weight, shingle_size=k)]
-                                            df_all_matches, all_time = main_joint.main(dataset_size, attribute_threshold, matching_method, matching_params)
-                                            false_positive, false_negative, true_positive, true_negative = add_results_estimation(df_all_matches, df_labeled_data)
-                                            experiment = {'dataset_size': dataset_size,
-                                                          'matching_attribute': matching_params[0].matching_attribute,# [0]should be fixed
-                                                          'attribute_threshold': matching_params[0].attribute_threshold,
-                                                          'matching_method': matching_params[0].matching_method,
-                                                          'hash_type': matching_params[0].hash_type,
-                                                          'shingles_size': matching_params[0].shingle_size,
-                                                          'hash_weight': matching_params[0].hash_weight,
-                                                          'signature_size': matching_params[0].signature_size,
-                                                          'bands_number': matching_params[0].bands_number,
-                                                          'total_time': all_time,
-                                                          'number_of_matches': len(df_all_matches),# 'minhash_time', 'sign_creation_time',
-                                                          'false_pos_rate': false_positive / (
-                                                                      false_positive + true_negative),
-                                                          'false_neg_rate': false_negative / (
-                                                                      false_negative + true_positive),
-                                                          'true_pos_rate': true_positive / (true_positive + false_negative),
-                                                          'true_neg_rate': true_negative / (true_negative + false_positive)
-                                                          }
-                                            df_results = df_results.append(experiment, ignore_index=True)
-                                    elif hash_type == 'token':
-                                        matching_params = [minhash_matching_params(matching_attribute, hash_type, hash_weight)]
-                                        df_all_matches, all_time = main_joint.main(dataset_size, attribute_threshold, matching_method, matching_params)
+                                if hash_type == 'token':
+                                    shingle_sizes = [0]
 
-                                        false_positive, false_negative, true_positive, true_negative = add_results_estimation(
-                                            df_all_matches, df_labeled_data)
-                                        experiment = {'dataset_size': dataset_size,
-                                                      'matching_attribute': matching_params[0].matching_attribute,# [0]should be fixed
-                                                      'attribute_threshold': matching_params[0].attribute_threshold,
-                                                      'matching_method': matching_params[0].matching_method,
-                                                      'hash_type': matching_params[0].hash_type,
-                                                      'shingles_size': matching_params[0].shingle_size,
-                                                      'hash_weight': matching_params[0].hash_weight,
-                                                      'signature_size': matching_params[0].signature_size,
-                                                      'bands_number': matching_params[0].bands_number,
-                                                      'total_time': all_time,
-                                                      'number_of_matches': len(df_all_matches),# 'minhash_time', 'sign_creation_time',
-                                                      'false_pos_rate': false_positive / (
-                                                              false_positive + true_negative),
-                                                      'false_neg_rate': false_negative / (
-                                                              false_negative + true_positive),
-                                                      'true_pos_rate': true_positive / (true_positive + false_negative),
-                                                      'true_neg_rate': true_negative / (true_negative + false_positive)
-                                                      }
-                                        df_results = df_results.append(experiment, ignore_index=True)
+                        for hash_type in hash_types:
+                            for hash_weight in hash_weights:
+                                for k in shingle_sizes:
+                                    for bands_number in bands_numbers:
+                                        for signature_size in signature_sizes:
+                                            matching_params = [attribute_matching_params(matching_attribute, matching_method, attribute_weight, hash_type, hash_weight, shingle_size=k, bands_number, signature_size)]
+
+                                            df_all_matches, all_time = main_joint.main(dataset_size, matching_params)
+
+                                            df_matches_estimation = pd.merge(df_all_matches, df_labeled_data, how='left', left_on=['doc_1', 'doc_2'], right_on=['id_x', 'id_y'])
+
+                                            for attribute_threshold in attribute_thresholds:
+                                                false_positive, false_negative, true_positive, true_negative = add_results_estimation(
+                                                    df_matches_estimation[df_matches_estimation.match_score > attribute_threshold], labeled_positive, labeled_negative, attribute_threshold)
+
+                                                experiment = {'dataset_size': dataset_size,
+                                                              'matching_attribute': matching_params[0].matching_attribute,# [0]should be fixed
+                                                              'attribute_threshold': matching_params[0].attribute_threshold,
+                                                              'matching_method': matching_params[0].matching_method,
+                                                              'hash_type': matching_params[0].hash_type,
+                                                              'shingles_size': matching_params[0].shingle_size,
+                                                              'hash_weight': matching_params[0].hash_weight,
+                                                              'signature_size': matching_params[0].signature_size,
+                                                              'bands_number': matching_params[0].bands_number,
+                                                              'total_time': all_time,
+                                                              'number_of_matches': len(df_all_matches),# 'minhash_time', 'sign_creation_time',
+                                                              'false_pos_rate': false_positive / (
+                                                                          false_positive + true_negative),
+                                                              'false_neg_rate': false_negative / (
+                                                                          false_negative + true_positive),
+                                                              'true_pos_rate': true_positive / (true_positive + false_negative),
+                                                              'true_neg_rate': true_negative / (true_negative + false_positive)
+                                                              }
+
+                                            df_results = df_results.append(experiment, ignore_index=True)
                                 #else: continue
     df_results.to_csv("df_results_{}.csv".format(str(datetime.datetime.now())))
     return df_results
