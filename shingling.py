044ed3533f894919837bc42fdcb1e99c968c950c
anna-atl <kiccoro@mail.ru>
2021-10-13 16:52:29 +0100

    edited names in shingling

--- shingling.py
+++ shingling.py
@@ -15,7 +15,7 @@ def create_shingles(doc, k):
     return shingles
 
 #creating shingles_dict
-def create_hashes(docs, hash_type, shingle_size, shingle_weight):
+def create_hashes(docs, shingle_type, shingle_size, shingle_weight):
     '''
     This function creates a list and dict of shingles
     :param texts:one string column of a df (comp names), which is going to be divided in shingles
@@ -23,23 +23,23 @@ def create_hashes(docs, hash_type, shingle_size, shingle_weight):
     :return: list of all shingles with order
     dict of shingles, shingle as a key, index of the shingle in the list as a value
     '''
-    hashes_set = set()#hash - hashed token or shingle
+    shingles_set = set()#hash - hashed token or shingle
     shingles_weights_dict = {} #token - unique word, value - tokens index
     k = shingle_size
 
     for doc in docs:
-        if hash_type == 'token':
-            hashes = [word for word in doc.split()]
-        elif hash_type == 'shingle':
-            hashes = create_shingles(doc, k)
-        elif hash_type == 'shingle words':
+        if shingle_type == 'token':
+            shingles = [word for word in doc.split()]
+        elif shingle_type == 'shingle':
+            shingles = create_shingles(doc, k)
+        elif shingle_type == 'shingle words':
             words = [word for word in doc.split()]
-            hashes = []
+            shingles = []
             for word in words:
-                hashes1 = create_shingles(word, k)
-                hashes.extend(hashes1)
-        for word_index, word in enumerate(reversed(hashes)):
-            hashes_set.add(word)
+                shingles_of_word = create_shingles(word, k)
+                shingles.extend(shingles_of_word)
+        for word_index, word in enumerate(reversed(shingles)):
+            shingles_set.add(word)
             if shingle_weight == 'weighted':
                 shingles_weights_dict.setdefault(word, []).append(word_index + 1)
     if shingle_weight == 'weighted':
@@ -49,10 +49,10 @@ def create_hashes(docs, hash_type, shingle_size, shingle_weight):
         avg = {key: 1/len(value) for key, value in shingles_weights_dict.items()}
         shingles_weights_dict.update(avg)
 
-    hashes_dict = dict(zip(hashes_set, range(len(hashes_set))))
-    hashes_list = [k for k in hashes_dict.keys()]
+    shingles_dict = dict(zip(shingles_set, range(len(shingles_set))))
+    shingles_list = [k for k in shingles_dict.keys()]
 
-    shingles_weights_list = [shingles_weights_dict[hash] for hash in hashes_list]
+    shingles_weights_list = [shingles_weights_dict[shingle] for shingle in shingles_dict]
 
     #put it in diff function
     try:
@@ -64,10 +64,10 @@ def create_hashes(docs, hash_type, shingle_size, shingle_weight):
     except:
         shingles_weights_list_normalized = []
 
-    return shingles_weights_list_normalized, hashes_dict
+    return shingles_weights_list_normalized, shingles_dict
 
 #converting docs to shingles
-def convert_docs_to_hashes(docs, hash_type, shingle_size, shingle_weight, shingles_weights_list, hashes_dict):
+def convert_docs_to_hashes(docs, shingle_type, shingle_size, shingle_weight, shingles_weights_list, shingles_dict):
     '''
     '''
     docs_shingled = [[] for i in range(len(docs))]
@@ -75,37 +75,37 @@ def convert_docs_to_hashes(docs, hash_type, shingle_size, shingle_weight, shingl
     k = shingle_size
 
     for doc_shingled, doc, shingles_weights_in_doc in zip(docs_shingled, docs, shingles_weights_in_docs):
-        if hash_type == 'token':
-            hashes = [word for word in doc.split()]
-        elif hash_type == 'shingle':
-            hashes = create_shingles(doc, k)
-        elif hash_type == 'shingle words':
+        if shingle_type == 'token':
+            shingles = [word for word in doc.split()]
+        elif shingle_type == 'shingle':
+            shingles = create_shingles(doc, k)
+        elif shingle_type == 'shingle words':
             words = [word for word in doc.split()]
-            hashes = []
+            shingles = []
             for word in words:
-                hashes1 = create_shingles(word, k)
-                hashes.extend(hashes1)
-        for word in hashes:
-            doc_shingled.append(hashes_dict[word])
+                shingles_of_word = create_shingles(word, k)
+                shingles.extend(shingles_of_word)
+        for word in shingle_type:
+            doc_shingled.append(shingles_dict[word])
 
         # and create weights of shingles in docs - list of dictionaries, key - shingle(index), value - weight (sum the weights if its several times)
         if shingle_weight == 'weighted':
-            for hash_position, hash_index in enumerate(reversed(doc_shingled)):
-                hash_in_doc_weight = (hash_position + 1)/len(doc_shingled) * shingles_weights_list[hash_index] #check if list is already created
-                shingles_weights_in_doc.setdefault(hash_index, []).append(hash_in_doc_weight)
+            for shingle_position, shingle_index in enumerate(reversed(doc_shingled)):
+                hash_in_doc_weight = (shingle_position + 1)/len(doc_shingled) * shingles_weights_list[shingle_index] #check if list is already created
+                shingles_weights_in_doc.setdefault(shingle_index, []).append(hash_in_doc_weight)
             shingles_weights_in_doc = {k: sum(v) for k, v in shingles_weights_in_doc.items()}
 
     return docs_shingled, shingles_weights_in_docs
 
-def main(docs, hash_type, shingle_size, shingle_weight):
+def main(docs, shingle_type, shingle_size, shingle_weight):
     start_time = time.time()
     print("Started creating shingles...")
-    shingles_weights_list, hashes_dict = create_hashes(docs, hash_type, shingle_size, shingle_weight)
+    shingles_weights_list, shingles_dict = create_hashes(docs, shingle_type, shingle_size, shingle_weight)
     print("Creating shingles took --- %s seconds ---" % (time.time() - start_time))
 
     start_time = time.time()
     print("Started converting docs to hashes...")
-    docs_shingled, shingles_weights_in_docs = convert_docs_to_hashes(docs, hash_type, shingle_size, shingle_weight, shingles_weights_list, hashes_dict)
+    docs_shingled, shingles_weights_in_docs = convert_docs_to_hashes(docs, shingle_type, shingle_size, shingle_weight, shingles_weights_list, shingles_dict)
     print("Converting docs to hashes took --- %s seconds ---" % (time.time() - start_time))
 
     return docs_shingled, shingles_weights_in_docs, shingles_weights_list
