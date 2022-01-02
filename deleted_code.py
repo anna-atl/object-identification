73328648b94c5d2061e329a5a81a9b2e9966b8de
Anna Atlasova <43668498+anna-atl@users.noreply.github.com>
2022-01-02 02:59:06 +0000

    started to fix shingling and adding weighting functions

---
+++ deleted_code.py
@@ -0,0 +1,4 @@
+docs_shingled[doc_index] = shingles
+doc_shingled.append(shingles_dict[shingle])  # fix the reversed
+
+for shingle in shingles:
--- shingling.py
+++ shingling.py
@@ -37,23 +37,89 @@ def create_shingles_dict(docs, shingle_type, shingle_size, shingle_weight):
     :return: list of all shingles with order
     dict of shingles, shingle as a key, index of the shingle in the list as a value
     '''
-    shingles_set = set()
-    shingles_weights_dict = {} #key - shingle, value - shingle weight
     k = shingle_size
+    all_shingles = {}
 
-    for doc in docs:
+    docs_shingled = [{} for i in range(len(docs))]
+    shingles_positions_in_docs = [{} for i in range(len(docs))]
+    shingles_frequency_in_docs = [{} for i in range(len(docs))]
+
+
+    #maybe to delete this one???
+    shingles_weights_dict = {} #key - shingle, value - shingle weight
+    shingles_weights_in_docs = [{} for i in range(len(docs))]
+
+    tf_in_docs = [{} for i in range(len(docs))]
+    shingles_frequency = {}
+
+    for doc_index, doc, shingles_positions_in_doc in zip(enumerate(docs), shingles_positions_in_docs):
         if shingle_type == 'token':
-            shingles = [word for word in doc.split()]
+            shingles_in_doc = [word for word in doc.split()]
         elif shingle_type == 'shingle':
-            shingles = create_shingles(doc, k)
+            shingles_in_doc = create_shingles(doc, k)
         elif shingle_type == 'shingle words':
             words = [word for word in doc.split()]
             shingles = []
             for word in words:
-                shingles_of_word = create_shingles(word, k)
-                shingles.extend(shingles_of_word)
-        for shingles_index_from_end, shingle in enumerate(reversed(shingles)):
-            shingles_set.add(shingle)
+                shingles_in_word = create_shingles(word, k)
+                shingles_in_doc.extend(shingles_in_word) #????
+        docs_shingled[doc_index] = shingles_in_doc
+        for shingle_position_in_doc, shingle_in_doc in enumerate(shingles_in_doc):
+            all_shingles.setdefault(shingle_in_doc, []).append(doc_index)
+            shingles_positions_in_doc.setdefault(shingle_in_doc, []).append(shingle_position_in_doc)
+
+    total_number_of_shingles = sum(values(all_shingles)) #not unique
+    if shingle_weight != "none":
+        for shingle, docs_with_this_shingle in all_shingles.items():
+            shingles_frequency[shingle] = len(docs_with_this_shingle)
+
+        for doc_index, shingles_in_doc, shingles_frequency_in_doc, shingles_positions_in_doc in zip(
+                enumerate(docs_shingled), shingles_frequency_in_docs, shingles_positions_in_docs):
+            for shingle_in_doc in shingles_in_doc:
+                shingles_frequency_in_doc[shingle_in_doc] = len(shingles_positions_in_doc[shingle_in_doc].values())
+            total_number_of_shingles_in_doc = len(shingles_in_doc)
+
+        for shingle, shingle_position_in_docs in shingles_positions_in_docs.items():
+            shingle_frequency[] =
+
+            len(shingles_positions_in_doc)
+
+
+
+
+            tf_in_doc[shingle_position_in_doc] = docs_shingled[doc_index]
+
+            how
+            many
+            shingles
+
+            shingles_positions_in_doc[shingle]
+
+            tf = shingle
+
+            docs_shingled[doc_index]
+
+        idf = {shingle, len(shingles_positions_in_doc) / len(shingles_positions_in_doc.value())
+        for k, v in shingles_positions_in_doc.items()}
+
+
+
+
+        for doc_index, doc in enumerate(docs):
+            shingles_weights_in_docs[doc_index].setdefault(shingle, []).append(shingles_index_from_end + 1)
+        
+
+
+
+        # and create weights of shingles in docs - list of dictionaries, key - shingle(index), value - weight (sum the weights if its several times)
+        for shingle_position_from_end, shingle_index in enumerate(reversed(doc_shingled)):
+                if shingle_in_doc_weight = (shingle_position_from_end + 1) / len(doc_shingled) * shingles_weights_dict[
+                            shingle_index]  # check if list is already created
+                        shingles_weights_in_doc.setdefault(shingle_index, []).append(shingle_in_doc_weight)
+        else:
+            shingles_weights_in_doc[shingle_index] = [1]
+
+
             if shingle_weight == 'weighted 1' or shingle_weight == 'weighted 2':
                 shingles_weights_dict.setdefault(shingle, []).append(shingles_index_from_end + 1)
 
@@ -73,11 +139,7 @@ def create_shingles_dict(docs, shingle_type, shingle_size, shingle_weight):
     return shingles_dict, shingles_weights_dict
 
 def create_shingled_docs(docs, shingle_type, shingle_size, shingle_weight, shingles_weights_dict, shingles_dict):
-    docs_shingled = [[] for i in range(len(docs))]
-    shingles_weights_in_docs = [{} for i in range(len(docs))]
-    k = shingle_size
 
-    for doc_shingled, doc, shingles_weights_in_doc in zip(docs_shingled, docs, shingles_weights_in_docs):
         if shingle_type == 'token':
             shingles = [word for word in doc.split()]
         elif shingle_type == 'shingle':
@@ -88,16 +150,7 @@ def create_shingled_docs(docs, shingle_type, shingle_size, shingle_weight, shing
             for word in words:
                 shingles_of_word = create_shingles(word, k)
                 shingles.extend(shingles_of_word)
-        for shingle in shingles:
-            doc_shingled.append(shingles_dict[shingle])
 
-        # and create weights of shingles in docs - list of dictionaries, key - shingle(index), value - weight (sum the weights if its several times)
-        for shingle_position_from_end, shingle_index in enumerate(reversed(doc_shingled)):
-            if shingle_weight == 'weighted 1' or shingle_weight == 'weighted 2':
-                shingle_in_doc_weight = (shingle_position_from_end + 1)/len(doc_shingled) * shingles_weights_dict[shingle_index] #check if list is already created
-                shingles_weights_in_doc.setdefault(shingle_index, []).append(shingle_in_doc_weight)
-            elif shingle_weight == 'normal':
-                shingles_weights_in_doc[shingle_index] = [1]
 
     return docs_shingled, shingles_weights_in_docs
 
